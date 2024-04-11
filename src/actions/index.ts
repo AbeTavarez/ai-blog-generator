95d1ab31d92317cb65f500101fb0a366805d1595
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-04-10 23:14:45 -0400

    chat completion stored

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,7 @@
 "use server";
 import OpenAI from "openai";
+import clientPromise from "@/db/mongoDb";
+import { revalidatePath } from "next/cache";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,15 +11,23 @@ export async function handleCompletion(formData: FormData) {
   const prompt = formData.get("user_prompt") as string;
   console.log(prompt);
 
-    try {
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: "user", content: prompt }],
-        model: "gpt-3.5-turbo",
-      });
-      console.log(completion.choices[0].message.content);
-      
-    } catch (e) {
-      console.log("Error with OPEN AI: ", e);
-    }
-  
+  // db
+  const client = await clientPromise;
+  const db = client.db("bloggenerator");
+
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: [{ role: "user", content: prompt }],
+      model: "gpt-3.5-turbo",
+    });
+    console.log(completion.choices[0].message.content);
+
+    const genBlog = completion.choices[0].message.content;
+
+    const blog = await db.collection("blogs").insertOne({ blog: genBlog });
+    console.log(blog);
+    revalidatePath("/blogs");
+  } catch (e) {
+    console.log("Error with OPEN AI: ", e);
+  }
 }
