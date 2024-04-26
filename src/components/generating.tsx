b6ea45bc15fb42c844d415f8ec7411afb051107e
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-04-25 23:31:44 -0400

    loading/generating component

--- src/components/generating.tsx
+++ src/components/generating.tsx
@@ -2,7 +2,7 @@ import { FaRobot } from "react-icons/fa";
 
 export default function Generating() {
   return (
-    <div className="flex justify-center items-center animate-pulse text-blue-300">
+    <div className="flex flex-1 justify-center items-center animate-pulse text-blue-300">
       <FaRobot size={42} />
       <div className="text-3xl ml-2">Generating...</div>
     </div>
